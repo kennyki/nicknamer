090e431ed4589e2518f7f7d3737638ab4d8bce4d
kennyki <>
2018-05-01 16:22:02 +0800

    support exclude config

--- index.js
+++ index.js
@@ -5,9 +5,20 @@ const list = db.list
 module.exports = {
   count,
   list,
-  get () {
+  get (config = {}) {
+    let list = this.list
+
+    if (config.exclude && config.exclude.length) {
+      const excludeMap = config.exclude.reduce((map, n) => {
+        map[n] = true
+        return map
+      }, {})
+
+      list = list.filter(n => !excludeMap[n])
+    }
+
     const randomIx = Math.floor(Math.random() * Math.floor(this.count))
-    const nickname = this.list[randomIx]
+    const nickname = list[randomIx]
 
     return nickname
   }
--- test.js
+++ test.js
@@ -1,11 +1,29 @@
 const nicknamer = require('./index')
 const generateCount = 10
+const firstBatch = []
 
-console.log(`Testing random generation of ${generateCount} nicknames:`)
+console.log('=============================================')
+console.log(`Testing generation of ${generateCount} nicknames:`)
 console.log('=============================================')
 
 for (let i = 0; i < generateCount; i++) {
-  console.log(nicknamer.get())
+  let nickname = nicknamer.get()
+  firstBatch.push(nickname)
+  console.log(nickname)
 }
 
 console.log('=============================================')
+console.log(`Testing generation of another ${generateCount} nicknames excluding the first batch:`)
+console.log('=============================================')
+
+for (let i = 0; i < generateCount; i++) {
+  let nickname = nicknamer.get({
+    exclude: firstBatch
+  })
+
+  if (firstBatch.indexOf(nickname) === -1) {
+    console.log(nickname)
+  } else {
+    console.error(`${nickname} (CLASHED)`)
+  }
+}
